6f2128d5b57dedf6348ff3ce12856ed65e2ba988
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-07-04 12:12:01 +0300

    Add basic style , functionality on Tabs

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,63 @@ import Buttons from './Buttons';
 import Tabs from './Tabs';
 import Cards from './Cards';
 import logo from '../assests/imgs/logo.png';
-import '../style/Layout.css'
+import '../style/Layout.css';
+import logoOne from '../assests/imgs/logo-1.png'
+import logoTwo from '../assests/imgs/logo-2.png'
+import logoThree from '../assests/imgs/logo-3.png'
+import logoFour from '../assests/imgs/logo-4.png'
+import logoFive from '../assests/imgs/logo-5.png'
+import logoSix from '../assests/imgs/logo-6.png'
+
 
 class Layout extends Component {
+    state = {
+        tabs: [
+        {
+            id: 1,
+            name: "",
+            logo: logoOne
+        },
+        {
+            id: 2,
+            name: "",
+            logo: logoTwo
+        }
+        ,
+        {
+            id: 3,
+            name: "",
+            logo: logoThree
+        }
+        ,{
+            id: 4,
+            name: "",
+            logo: logoFour
+        },
+        {
+            id: 5,
+            name: "",
+            logo: logoFive
+        },
+        {
+            id: 6,
+            name: "",
+            logo: logoSix
+        }
+    ],
+        posts: {
+            id: 0 ,
+            companyName: "",
+            text: "",
+            socialPlatform: ""
+        }
+    }
+
+
     render(){
+        const tabs = this.state.tabs.map(tab => (
+            <Tabs key={tab.id} logo={tab.logo} name={tab.name} />
+        ))
         return(
             <div className="Layout">
                 <div className="row h-100">
@@ -17,7 +70,7 @@ class Layout extends Component {
                         </div>
                         <div className="row h-100">
                             <div className="Layout-tabs-area col-3 border border-primary">
-                                <Tabs />
+                                {tabs}
                             </div>
                             <div className="Layout-btns-area col-9 ">
                                 <Buttons />
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,11 +1,28 @@
-import React , {Component} from 'react'
+import React , {Component} from 'react';
+import '../style/Tabs.css';
+import logo from '../assests/imgs/logo-1.png'
 
 class Tabs extends Component {
+static defaultProps = {
+    logo: logo
+}
+
+    state = {
+        isClicked: false
+    }
+
+    handleClick = ({target})=>{
+        this.setState({isClicked: !this.state.isClicked});
+        
+    }
+
     render(){
+        const tabClicked = this.state.isClicked ? "Tabs-clicked" : "Tabs-notCliked";
+        const hiddenDiv = this.state.isClicked ? "Tabs-div-show" : "Tabs-div-hide";
         return(
-            <div>
-                {/* <h1>Sidebar</h1>
-                <p> brand switching tab </p> */}
+            <div className="Tabs">
+                <div className={`Tabs-hidden ${hiddenDiv}`}></div>
+                <img src={this.props.logo} onClick={this.handleClick} className={tabClicked} />
             </div>
         )
     }
